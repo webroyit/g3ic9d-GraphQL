498dc94b40cd11d2caec239df130668cd0625870
royweb <xytank@gmail.com>
2019-01-28 19:14:57 -0500

    Use vadlidator and add validation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,27 @@
+const validator = require('validator');
+
 const Account = require('../models/account');
 
 // the logic
 module.exports = {
     // args is a object
     createAccount: async function({ accountData }, req){
+        const errors = [];
+        if(validator.isEmpty(accountData.username)){
+            errors.push({ message: "Username cannot be empty"})
+        }
+        if(validator.isEmpty(accountData.password) || !validator.isLength(accountData.password, { min: 6 })){
+            errors.push({ message: "Password must be at least 6 characters long"})
+        }
+        if(validator.isEmpty(accountData.bio) || !validator.isLength(accountData.bio, { min: 5 })){
+            errors.push({ message: "Bio must be at least 5 characters long"})
+        }
+
+        if(errors.length > 0){
+            const error = new Error("Error alert");
+            throw error;
+        }
+
         const checkUsername = await Account.findOne({username: accountData.username});
         // check if that username exist
         if(checkUsername){
