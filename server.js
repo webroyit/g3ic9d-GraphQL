eea306e4182293fd7b8b397ea47d8f8ade313120
royweb <xytank@gmail.com>
2019-02-12 15:27:25 -0500

    Fix CORS issues

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const db = require('./keys').mongoURI;
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+});
+
 app.use('/graphql', expressGraphql({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
