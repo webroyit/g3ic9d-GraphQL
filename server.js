632181eb2166a542a6a4257225659e0d529c47fe
royweb <xytank@gmail.com>
2019-02-12 15:41:47 -0500

    Fix the problem of sending option request to graphql

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+    // express graphql only accept get or post request
+    if (req.method === 'OPTIONS') {
+        // prevent the execution of option request
+      return res.sendStatus(200);
+    }
+    next();
 });
 
 app.use('/graphql', expressGraphql({
